618c64b2dee25d6b12db4f066cff199824907ba9
chandrawat <saksham.mist@gmail.com>
2019-07-26 12:11:00 -0400

    Added @Autowired

--- SpringAnnotationPractise/src/main/java/com/saksham/SpringAnnotationPractice/CollegeConfiguration.java
+++ SpringAnnotationPractise/src/main/java/com/saksham/SpringAnnotationPractice/CollegeConfiguration.java
@@ -1,17 +1,22 @@
 package com.saksham.SpringAnnotationPractice;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 //@ComponentScan(basePackages = "com.saksham.SpringAnnotationPractice ")
 public class CollegeConfiguration {
 	
-	
+
 	@Bean
-	public College collegeBeanMethod() {		//method name will be bean ID
-		College college=new College();
+	public Principal principalBeanMethod() {
+		return new Principal();
+	}
+	
+	
+	@Bean(name= "colBean")		// for multiple name use=> @Bean(name={"abc","bcd"})
+	public College collegeBeanMethod() {		//by default method name will be bean ID
+		College college=new College(principalBeanMethod());
 		return college;
 	}
 
